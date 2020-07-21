7b10c54b391c2be13e9dd2473bd209b1f77262d3
j3ffh95 <j3ffh95@gmail.com>
2020-07-21 16:42:32 -0400

    change the github class and added some instance properties

--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class Github {
   constructor() {
     this.client_id = "9aa14c69908b5f04c43c";
     this.client_secret = "2aaff10caf6d1d092bcddbbb9914d086630f67fa";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
