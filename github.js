b97a3dcc7f4ccd8ecbed6dfd7752299bc89039e8
j3ffh95 <j3ffh95@gmail.com>
2020-07-21 15:13:17 -0400

    added getUser method to github class

--- github.js
+++ github.js
@@ -3,4 +3,16 @@ class Github {
     this.client_id = "9aa14c69908b5f04c43c";
     this.client_secret = "2aaff10caf6d1d092bcddbbb9914d086630f67fa";
   }
+
+  async getUser() {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+
+    return {
+      profile,
+    };
+  }
 }
